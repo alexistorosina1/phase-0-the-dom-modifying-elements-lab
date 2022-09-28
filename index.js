795a54627c7b9ac0d43b90f991cf16fc96f8dcb4
Alexis Torosina <alexistorosina1@gmail.com>
2022-09-28 18:41:11 -0400

    commit tree 81a2e035c54b046ed399cb8fdddb9663f198145e

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main = document.querySelector('main')
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "Alexis is the champion"
